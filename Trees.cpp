d51c6cb4df7f8edc863d4d10519b4a31604614ec
Ashok <68994450+AshokDon@users.noreply.github.com>
2021-06-07 22:08:42 +0530

    Add files via upload

---
+++ Trees.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int key;
+    Node *left;
+    Node *right;
+    Node(int k)
+    {
+        key=k;
+        left=right=NULL;
+    }
+};
+
+int main() {
+    //we represent an empty tree as null 
+    //Node *root=NULL;
+	Node *root=new Node(10);
+	root->left=new Node(20);
+	root->right=new Node(30);
+	root->left->left=new Node(40);
+	return 0;
+}
